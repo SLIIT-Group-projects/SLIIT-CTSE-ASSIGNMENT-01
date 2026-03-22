ab5cbabb137a9eebd2df749acb791da72acca891
Siluni Rupasinghe <133697974+silunirupasinghe@users.noreply.github.com>
2026-03-22 20:24:36 +0530

    correction

--- backend/api-gateway/src/config.js
+++ backend/api-gateway/src/config.js
@@ -4,9 +4,9 @@ dotenv.config();
 
 module.exports = {
   port: Number(process.env.PORT || 4000),
-  authServiceUrl: process.env.AUTH_SERVICE_URL || 'http://auth-service:4001',
-  appointmentServiceUrl: process.env.APPOINTMENT_SERVICE_URL || 'http://appointment-service:4002',
-  doctorServiceUrl: process.env.DOCTOR_SERVICE_URL || 'http://doctor-service:4003',
-  labServiceUrl: process.env.LAB_SERVICE_URL || 'http://lab-service:4004',
-  billingServiceUrl: process.env.BILLING_SERVICE_URL || 'http://billing-service:4005',
+  authServiceUrl: process.env.AUTH_SERVICE_URL || 'https://auth-service-0vqx.onrender.com',
+  appointmentServiceUrl: process.env.APPOINTMENT_SERVICE_URL || 'https://appointment-service-t2lw.onrender.com',
+  doctorServiceUrl: process.env.DOCTOR_SERVICE_URL || 'https://doctor-service-7w9h.onrender.com',
+  labServiceUrl: process.env.LAB_SERVICE_URL || 'https://sliit-ctse-assignment-01-7.onrender.com',
+  billingServiceUrl: process.env.BILLING_SERVICE_URL || 'https://billing-service-60xe.onrender.com',
 };
